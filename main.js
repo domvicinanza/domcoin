e7e1ed57dc6db7845d07cc9f573a2c52b69f48c4
Domenico Vicinanza <domenico@fst-cat-mbpr-domenico.anglia.local>
2022-08-22 15:02:28 +0100

    First commit of domcoin

---
+++ main.js
@@ -0,0 +1,40 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash=''){
+        this.index=index;
+        this.timestamp=timestamp;
+        this.data=data;
+        this.previousHash=previousHash;
+        this.hash=this.calculateHash();
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();    
+    }
+}
+    class Blockchain{
+        constructor(){
+            this.chain = [this.createGenesisBlock()];
+        }
+
+        createGenesisBlock(){
+            return new Block(0,"01/01/2022","Genesis Block","0");
+        }
+
+        getLatestBlock(){
+            return this.chain[this.chain.length -1];
+        }
+
+        addBlock(newBlock){
+            newBlock.previousHash=this.getLatestBlock().hash;
+            newBlock.hash=newBlock.calculateHash();
+            this.chain.push(newBlock);
+        }
+    }
+    
+
+let dvcoin= new Blockchain;
+dvcoin.addBlock(new Block(1,"10/08/2022",{amount: 4}));
+dvcoin.addBlock(new Block(2,"12/08/2022",{amount: 10}));
+console.log(JSON.stringify(dvcoin, null, 4));
